3fc678b0d5b913163cb3a716763d9ca561a8852b
Joao Ferreira <joao.m.d.santos.ferreira@gmail.com>
2025-12-14 11:01:41 +0000

    Update architecture target. Add freetype dependency.

--- components/clayZigESP32/build.zig
+++ components/clayZigESP32/build.zig
@@ -10,26 +10,25 @@ pub fn build(b: *std.Build) void {
             .abi = .ilp32,
             .os_tag = .freestanding,
             .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
-        }; // march is rv32imc
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
+            .cpu_features_add = std.Target.riscv.featureSet(&.{
+                .zicsr,
+                .zifencei,
+                .zmmul,
+                .zaamo,
+                .zalrsc,
+                .a,
+                .f,
+                .d,
+                .c,
+                .m,
+            }),
+        };
+
     const target = b.standardTargetOptions(.{ .default_target = target_query });
 
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
     const optimize = b.standardOptimizeOption(.{ .preferred_optimize_mode = .ReleaseSafe });
 
-    // This creates a "module", which represents a collection of source files alongside
-    // some compilation options, such as optimization mode and linked system libraries.
-    // Every executable or library we compile will be based on one or more modules.
     const lib_mod = b.createModule(.{
-        // `root_source_file` is the Zig "entry point" of the module. If a module
-        // only contains e.g. external object files, you can make this `null`.
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
@@ -50,17 +49,22 @@ pub fn build(b: *std.Build) void {
     });
     lib_mod.addImport("zclay", zclay_dep.module("zclay"));
 
-    // Now, we will create a static library based on the module we created above.
-    // This creates a `std.Build.Step.Compile`, which is the build step responsible
-    // for actually invoking the compiler.
+    const freetype = b.dependency("zig_freetype2", .{
+        .target = target,
+        .optimize = optimize,
+    });
+    const freetype_module = freetype.module("freetype");
+    freetype_module.addSystemIncludePath(.{ .cwd_relative = "C:/Espressif/tools/riscv32-esp-elf/esp-14.2.0_20241119/riscv32-esp-elf/picolibc/include" });
+    freetype_module.addLibraryPath(.{ .cwd_relative = "C:/Espressif/tools/riscv32-esp-elf/esp-14.2.0_20241119/riscv32-esp-elf/picolibc/riscv32-esp-elf/lib/rv32imac_zicsr_zifencei/ilp32/" });
+    freetype_module.linkSystemLibrary("c", .{ .needed = true, .preferred_link_mode = .static, .weak = false });
+
+    lib_mod.addImport("freetype2", freetype_module);
+
     const lib = b.addLibrary(.{
         .linkage = .static,
         .name = "clayZigESP32",
         .root_module = lib_mod,
     });
 
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
     b.installArtifact(lib);
 }
